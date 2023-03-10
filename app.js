e410143de059c90662b4c5b8f718e547e2e4a2be
Stamba1337 <stamba1337@gmail.com>
2023-03-10 20:42:33 +0200

    daasa

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-      button.addEventListener("click", function() {
-        document.querySelector(".active-btn").classList.remove("active-btn");
-        this.classList.add("active-btn");
-        document.querySelector(".active").classList.remove("active");
-        document.getElementById(button.dataset.id).classList.add("active");
-      })
-    });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-      document.body.classList.toggle("light-mode");
+  [...document.querySelectorAll(".control")].forEach(button => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     })
-  
-    function sendEmail() {
-      var name = document.getElementById("name-input").value;
-      var email = document.getElementById("email-input").value;
-      var subject = document.getElementById("subject-input").value;
-      var message = document.getElementById("message-input").value;
-      var mailtoLink = "mailto:stamba1337@gmail.com" +
-        "?subject=" + encodeURIComponent(subject) +
-        "&body=" + encodeURIComponent("Name: " + name + "\nEmail: " + email + "\n\n" + message);
-      window.location.href = mailtoLink;
-    }
-    
-    document.querySelector(".main-btn").addEventListener("click", function() {
-      sendEmail();
-    });
-  })();
-  
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  })
+
+  const birthdate = new Date('2004-09-16');
+
+  // Calculate the age
+  const age = Math.floor((Date.now() - birthdate) / (365.25 * 24 * 60 * 60 * 1000));
+
+  // Update the HTML
+  const ageElement = document.querySelector('.large-text');
+  ageElement.textContent = age;
+})();
