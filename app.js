3a516b8c42ecb003aecd29b5eda6c9e402b16cfb
Stanimir Lambov <stamba1337@gmail.com>
2023-10-11 09:22:39 +0300

    timeline rework

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 (function () {
+
+  // Utility Functions
+  function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+      const context = this, args = arguments;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  // --------------------- SCROLL & CLICK BUTTON ACTIVATION ---------------------
   const controls = document.querySelectorAll(".control");
   const sections = document.querySelectorAll("section");
 
@@ -7,83 +25,60 @@
     let activeSection = null;
 
     sections.forEach((section) => {
-      const sectionTop = section.offsetTop - 42;
+      const sectionTop = section.offsetTop - 42; // Consider using a descriptive constant for numbers
       const sectionHeight = section.offsetHeight;
-
-      if (
-        scrollPosition >= sectionTop &&
-        scrollPosition < sectionTop + sectionHeight
-      ) {
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
         activeSection = section.getAttribute("id");
       }
     });
 
     controls.forEach((button) => {
-      const buttonId = button.getAttribute("data-id");
-
-      if (buttonId === activeSection) {
-        button.classList.add("active-btn");
-      } else {
-        button.classList.remove("active-btn");
-      }
+      button.classList.toggle("active-btn", button.getAttribute("data-id") === activeSection);
     });
   }
 
-  window.addEventListener("scroll", setActiveButton);
-  setActiveButton();
-
-  controls.forEach((button) => {
-    button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
-    });
-  });
-
+  // --------------------- THEME TOGGLE ---------------------
   document.querySelector(".theme-btn").addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
   });
 
-  const smoothScrollLinks = document.querySelectorAll(".smooth-scroll");
-
-  for (let link of smoothScrollLinks) {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      const options = {
-        behavior: "smooth",
-        block: "start",
-        inline: "nearest",
-      };
-      target.scrollIntoView(options);
-    });
-  }
-  //loader
-  window.addEventListener('load', function () {
-    gsap.to("#cartridge", {
-      duration: 1.25,
-      y: "+=57",
-      onComplete: revealWebsite
-    });
-
-    function revealWebsite() {
-      gsap.to("#loader", {
-        duration: 0.5,
-        opacity: 0,
-        onComplete: () => {
-          document.getElementById("loader").style.display = "none";
-        }
+  // --------------------- SMOOTH SCROLL ---------------------
+  function initSmoothScroll() {
+    const smoothScrollLinks = document.querySelectorAll(".smooth-scroll");
+    for (let link of smoothScrollLinks) {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
       });
     }
+  }
 
-  });
-//documents
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      // Check if any required fields are empty
+  // --------------------- WEBSITE LOADER ANIMATION ---------------------
+  function initLoaderAnimation() {
+    window.addEventListener('load', function () {
+      gsap.to("#cartridge", {
+        duration: 1.00,
+        y: "+=57",
+        onComplete: () => {
+          gsap.to("#loader", {
+            duration: 0.25,
+            opacity: 0,
+            onComplete: () => {
+              document.getElementById("loader").style.display = "none";
+            }
+          });
+        }
+      });
+    });
+  }
+
+  // --------------------- AJAX CONTACT FORM SUBMISSION ---------------------
+  function initContactFormAjaxSubmission() {
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
       var emailInput = document.querySelector('input[name="email"]');
       var messageInput = document.querySelector('textarea[name="message"]');
-      if (emailInput.value.trim() === "" || messageInput.value.trim() === "") {
+      if (!emailInput.value.trim() || !messageInput.value.trim()) {
         event.preventDefault(); // Prevent form submission
         showMessage("Please fill in all required fields.", "error");
         return;
@@ -91,18 +86,15 @@
 
       event.preventDefault(); // Prevent default form submission
 
-      var form = event.target;
-      var formData = new FormData(form);
-
-      // Send form data using AJAX
+      var formData = new FormData(this);
       var xhr = new XMLHttpRequest();
-      xhr.open("POST", form.action, true);
+      xhr.open("POST", this.action, true);
       xhr.setRequestHeader("Accept", "application/json");
       xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
           if (xhr.status === 200) {
             showMessage("Form submitted successfully.", "success");
-            form.reset(); // Reset the form
+            event.target.reset();
           } else {
             showMessage("Failed to submit form. Please try again.", "error");
           }
@@ -111,59 +103,50 @@
       xhr.send(formData);
     });
 
-  // Function to display messages
-  function showMessage(message, type) {
-    var messageContainer = document.getElementById("message-container");
-    messageContainer.innerHTML = ""; // Clear previous messages
+    function showMessage(message, type) {
+      var messageContainer = document.getElementById("message-container");
+      messageContainer.innerHTML = ""; // Clear previous messages
 
-    var messageElement = document.createElement("div");
-    messageElement.classList.add("message-" + type);
+      var messageElement = document.createElement("div");
+      messageElement.classList.add("message-" + type);
 
-    if (type === "success") {
-      var successIcon = document.createElement("i");
-      successIcon.classList.add("fas", "fa-check", "success-icon");
-      messageElement.appendChild(successIcon);
-    }
+      if (type === "success") {
+        var successIcon = document.createElement("i");
+        successIcon.classList.add("fas", "fa-check", "success-icon");
+        messageElement.appendChild(successIcon);
+      }
 
-    messageElement.appendChild(document.createTextNode(message));
-    messageContainer.appendChild(messageElement);
+      messageElement.appendChild(document.createTextNode(message));
+      messageContainer.appendChild(messageElement);
 
-    if (type === "success") {
-      var sendButton = document.querySelector('button[type="submit"]');
-      sendButton.insertAdjacentElement("afterend", messageElement);
-
-      // Remove message and icon after 3 seconds
-      setTimeout(function () {
-        messageElement.remove();
-      }, 2000);
+      if (type === "success") {
+        setTimeout(() => messageElement.remove(), 2000);
+      }
     }
   }
 
-  document.addEventListener('DOMContentLoaded', function () {
-
-    // Calculate the age logic
-    const birthdate = new Date("2004-09-16");
-    const age = Math.floor(
-      (Date.now() - birthdate) / (365.25 * 24 * 60 * 60 * 1000)
-    );
-
-    // Update the HTML
+  // --------------------- OTHER EFFECTS & LOGIC ---------------------
+  function initOtherEffects() {
+    // Calculate Age Logic
     const ageElement = document.querySelector(".large-text");
+    const birthdate = new Date("2004-09-16");
+    const age = Math.floor((Date.now() - birthdate) / (365.25 * 24 * 60 * 60 * 1000));
     ageElement.textContent = age;
 
     // Light circle effect for about-item
     document.getElementById("cards").onmousemove = (e) => {
       for (const card of document.getElementsByClassName("about-item")) {
-        const rect = card.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
-
+        const { left, top } = card.getBoundingClientRect(),
+          x = e.clientX - left,
+          y = e.clientY - top;
         card.style.setProperty("--mouse-x", `${x}px`);
         card.style.setProperty("--mouse-y", `${y}px`);
       }
     };
+  }
 
-    // Progress bars animation with Intersection Observer
+  // --------------------- PROGRESS BARS ANIMATION ---------------------
+  function initProgressBarAnimation() {
     const skillsSection = document.querySelector('.about-stats');
     let animated = false;
 
@@ -211,12 +194,79 @@
       }
     };
 
+    // Set up the Intersection Observer
+    const observerOptions = {
+      root: null,
+      threshold: 0.1  // At least 10% of the target is visible
+    };
 
-    const observer = new IntersectionObserver(animateBars, {
-      threshold: 0.3
-    });
-
+    const observer = new IntersectionObserver(animateBars, observerOptions);
     observer.observe(skillsSection);
+  }
+
+  function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Add the animation class to the target element
+        entry.target.classList.add('animated');
+
+        // Unobserve the element after animating (optional, but prevents unnecessary checks)
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+  };
+
+  const observer = new IntersectionObserver(handleIntersection, options);
+  document.querySelectorAll('.animate-on-scroll').forEach(el => observer.observe(el));
+
+
+  // Get the elements
+  var elements = document.querySelectorAll('.content');
+  console.log("elements --- ", elements);
+  // Function to check for fade effect on scroll and resize
+  function checkForFade() {
+    let windowHeight = window.innerHeight;
+  
+    elements.forEach(function(element) {
+      let elementHeight = element.offsetHeight;
+      let elementOffset = element.getBoundingClientRect().top;
+      let elementBottom = elementOffset + elementHeight;
+  
+      if (elementBottom > windowHeight && elementOffset < windowHeight - 650) {
+        element.classList.add('non-focus');
+      } else {
+        element.classList.remove('non-focus');
+      }
+    });
+  }
+  
+
+  // Add event listeners for scroll and resize and call the checkForFade function
+  window.addEventListener('scroll', checkForFade);
+  window.addEventListener('resize', checkForFade);
+
+  // Trigger the scroll event on initial load
+  window.dispatchEvent(new Event('scroll'));
+  window.addEventListener("scroll", debounce(setActiveButton));
+  controls.forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+    });
   });
 
+  document.addEventListener('DOMContentLoaded', function () {
+    checkForFade();
+    initSmoothScroll();
+    // initLoaderAnimation();
+    initContactFormAjaxSubmission();
+    initOtherEffects();
+    initProgressBarAnimation();
+  });
 })();
